6075a6fcf35c0b07556fc8e86ddda452bfed062d
Jan Kubalek <janmkubalek@gmail.com>
2021-07-12 14:16:35 +0200

    Build script added

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+VERSION=v3.9.1
+REPO_PATH=repo
+BUILD_PATH="${REPO_PATH}/_build"
+
+git clone git@github.com:nlohmann/json.git "${REPO_PATH}"
+
+mkdir -p "${BUILD_PATH}"
+pushd "${BUILD_PATH}"
+	git checkout ${VERSION}
+	cmake -DJSON_BuildTests=OFF \
+		-DCMAKE_INSTALL_PREFIX=INSTALL \
+		-DJSON_MultipleHeaders=ON \
+		../
+	make
+	make install
+	pushd INSTALL
+		zip -r libnlohmann-json-dev_${VERSION}_any.zip ./*
+	popd
+popd
+
+mv ${BUILD_PATH}/INSTALL/*.zip ./
